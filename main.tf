26e89f53d07b60af762fd211f48ab3627435b3bf
faijan74 <107401628+faijan74@users.noreply.github.com>
2022-06-29 22:08:27 -0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,73 @@
+resource "azurerm_resource_group" "example"
+ {
+  name     = "LoadBalancerRG"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "example" 
+{
+  name                = "PublicIPForLB"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "example" 
+{
+  name                = "TestLoadBalancer"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "example" 
+{
+  loadbalancer_id = azurerm_lb.example.id
+  name = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_rule" "example" 
+{
+  resource_group_name            = azurerm_resource_group.example.name
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "RDPAccess"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_outbound_rule" "example" 
+{
+
+  loadbalancer_id         = azurerm_lb.example.id
+  name                    = "OutboundRule"
+  protocol                = "Tcp"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+
+  frontend_ip_configuration 
+  {
+    name = "PublicIPAddress"
+  }
+}
+
+resource "azurerm_lb_probe" "example" 
+{
+  loadbalancer_id = azurerm_lb.example.id
+  name            = "ssh-running-probe"
+  port            = 22
+}
+
+resource "azurerm_lb_rule" "example" 
+{
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
